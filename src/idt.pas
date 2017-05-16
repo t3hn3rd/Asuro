a0b4639ba90b95ca27926a109bd9cd5c68a572b0
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-16 23:05:26 +0000

    idt
    git-svn-id: https://spexeah.com:8443/svn/Asuro@35 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/idt.pas
+++ src/idt.pas
@@ -8,8 +8,23 @@ uses
 
 type
     TIDT_Entry = bitpacked record
-        
+        base_low  : uint16;
+        selector  : uint16;
+        always_0  : uint8;
+        flags     : uint8;
+        base_high : uint16; 
     end;
+    PIDT_Entry = ^TIDT_Entry;
+
+    TIDT_Pointer = bitpacked record
+        limit : uint16;
+        base  : uint32;
+    end;
+    PIDT_Pointer = ^TIDT_Pointer;
+
+var
+    IDT : Array [0..255] of TIDT_Entry;
+    IDT_Pointer : TIDT_Pointer;
 
 implementation
 
