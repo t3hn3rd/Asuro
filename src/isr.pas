446e82646cbbae47f1d2e739cba0705c4ec23f10
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-17 13:45:59 +0000

    Commented ISRs Registered ISR32 & 49.
    git-svn-id: https://spexeah.com:8443/svn/Asuro@57 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/isr.pas
+++ src/isr.pas
@@ -14,9 +14,8 @@ interface
 uses
     ISR0,  ISR1,  ISR2,  ISR3,  ISR4,  ISR5,  ISR6,  ISR7,  ISR8, ISR9, 
     ISR10, ISR11, ISR12, ISR13, ISR14, ISR15, ISR16, ISR17, ISR18, 
-    ISR33;
-
-    
+    ISR32, ISR33,
+    ISR49;    
 
 procedure init();
 
@@ -24,27 +23,29 @@ implementation
 
 procedure init();
 begin
-    ISR0.register();
-    ISR1.register();
-    ISR2.register();
-    ISR3.register();
-    ISR4.register();
-    ISR5.register();
-    ISR6.register();
-    ISR7.register();
-    ISR8.register();
-    ISR9.register();
-    ISR10.register();
-    ISR11.register();
-    ISR12.register();
-    ISR13.register();
-    ISR14.register();
-    ISR15.register();
-    ISR16.register();
-    ISR17.register();
-    ISR18.register();
+    ISR0.register();  // Divide-By-Zero
+    ISR1.register();  // Debug
+    ISR2.register();  // Non-Maskable Inturrupt
+    ISR3.register();  // Breakpoint
+    ISR4.register();  // Detected Overflow
+    ISR5.register();  // Out of Bounds
+    ISR6.register();  // Invalid OPCode
+    ISR7.register();  // No Coprocessor
+    ISR8.register();  // Double Fault
+    ISR9.register();  // CP Segment Overrun
+    ISR10.register(); // Bad TSS
+    ISR11.register(); // Segment not Present
+    ISR12.register(); // Stack Fault
+    ISR13.register(); // General Protection Fault
+    ISR14.register(); // Page Fault
+    ISR15.register(); // Unknown Interrupt
+    ISR16.register(); // CP Fault
+    ISR17.register(); // Alignment Check
+    ISR18.register(); // Machine Check
 
-    ISR33.register();
+    ISR32.register(); // 55ms Timer
+    ISR33.register(); // Keyboard
+    ISR49.register(); // 1024/s Timer
 end;
 
 end.
