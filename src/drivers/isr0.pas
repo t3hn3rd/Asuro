bc962029905c0b72f4f0028c79e7ef0e77916596
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-17 17:31:38 +0000

    ISR Hooking.
    git-svn-id: https://spexeah.com:8443/svn/Asuro@76 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/drivers/isr0.pas
+++ src/drivers/isr0.pas
@@ -33,7 +33,7 @@ var
 begin
     CLI;
     for i:=0 to MAX_HOOKS-1 do begin
-        if uint32(Hooks[i]) <> 0 then Hooks[i](nil);
+        if uint32(Hooks[i]) <> 0 then Hooks[i](void(0));
     end;
     console.writestringln('Divide by Zero Exception.');
     util.halt_and_catch_fire;
