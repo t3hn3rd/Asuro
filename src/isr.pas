a4274e411b8a7c1df5e10e2cbda503634bc1e33e
aaron <aaron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-17 13:53:50 +0000

    git-svn-id: https://spexeah.com:8443/svn/Asuro@59 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/isr.pas
+++ src/isr.pas
@@ -15,7 +15,8 @@ uses
     ISR0,  ISR1,  ISR2,  ISR3,  ISR4,  ISR5,  ISR6,  ISR7,  ISR8, ISR9, 
     ISR10, ISR11, ISR12, ISR13, ISR14, ISR15, ISR16, ISR17, ISR18, 
     ISR32, ISR33,
-    ISR49;    
+    ISR49,
+    irq;    
 
 procedure init();
 
@@ -23,6 +24,7 @@ implementation
 
 procedure init();
 begin
+    irq.init();
     ISR0.register();  // Divide-By-Zero
     ISR1.register();  // Debug
     ISR2.register();  // Non-Maskable Inturrupt
