7acc4046e374c2a1dc286ff794ceb4b6bfcfcad0
aaron <aaron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-17 13:50:51 +0000

    git-svn-id: https://spexeah.com:8443/svn/Asuro@58 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

---
+++ src/irq.pas
@@ -0,0 +1,30 @@
+{ ************************************************
+  * Asuro
+  * Unit: irq
+  * Description: mapping IRQs
+  ************************************************
+  * Author: Aaron Hance
+  * Contributors:
+  ************************************************ }
+
+unit irq;
+
+interface
+
+procedure init();
+
+implementation
+
+procedure init();
+
+outb($0020, $11);
+outb($00A0, $11);
+outb($0021, 32);
+outb($00A1, 40);
+outb($0021, 4);
+outb($00A1, 2);
+begin
+
+end;
+
+end.
