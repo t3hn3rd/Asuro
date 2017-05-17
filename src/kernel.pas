d6697f0e30b5d054df2d76e04d169fd8c3a68195
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-17 15:12:59 +0000

    See last.
    git-svn-id: https://spexeah.com:8443/svn/Asuro@67 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/kernel.pas
+++ src/kernel.pas
@@ -62,13 +62,7 @@ begin
      console.writeint(((mbinfo^.mem_upper + 1000) div 1024) +1);
      console.writestringln('MB');
      console.setdefaultattribute(console.combinecolors(lYellow, Black));
-     //asm INT 3 end;
      util.halt_and_dont_catch_fire;
-     {while true do begin
-          c:= keyboard.get_scancode;
-          console.writehexln(c);  
-     end;
-     util.halt_and_catch_fire;}
 end;
  
 end.
