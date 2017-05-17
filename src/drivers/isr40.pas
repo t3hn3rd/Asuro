f8399ecace7ea411f67e0a1a9c0bc0f4b9087f35
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-17 17:34:38 +0000

    More ISR Hooks.
    git-svn-id: https://spexeah.com:8443/svn/Asuro@77 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/drivers/isr40.pas
+++ src/drivers/isr40.pas
@@ -4,7 +4,7 @@
   * Description: 1024/s Timer interrupt
   ************************************************
   * Author: Aaron Hance
-  * Contributors: 
+  * Contributors: K Morris
   ************************************************ }
 
 unit isr40;
@@ -17,26 +17,57 @@ uses
     isr_types,
     IDT;
 
-//var
-    //procedure_ptr : pp_void = nil;
-
 procedure register();
+procedure hook(hook_method : uint32);
+procedure unhook(hook_method : uint32);
 
 implementation
 
+var
+    Hooks : Array[1..MAX_HOOKS] of pp_hook_method;
+
 procedure Main; interrupt; //IRQ0, called 1024 times a second.
+var
+    i : integer;
+
 begin
-    console.writestringln('helo3');
-    //if(procedure_ptr <> nil) then begin
-    //    procedure_ptr();
-    //end;
+    for i:=0 to MAX_HOOKS-1 do begin
+        if uint32(Hooks[i]) <> 0 then Hooks[i](void(18));
+    end;
     outb($A0, $20);
     outb($20, $20);
 end;
 
 procedure register();
 begin
+    memset(uint32(@Hooks[0]), 0, sizeof(pp_hook_method)*MAX_HOOKS);
     IDT.set_gate(40, uint32(@Main), $08, ISR_RING_0);
+end;
+
+procedure hook(hook_method : uint32);
+var
+    i : uint32;
+
+begin
+    for i:=0 to MAX_HOOKS-1 do begin
+        if uint32(Hooks[i]) = hook_method then exit;
+    end;
+    for i:=0 to MAX_HOOKS-1 do begin
+        if uint32(Hooks[i]) = 0 then begin
+            Hooks[i]:= pp_hook_method(hook_method);
+            exit;
+        end;
+    end;
+end;
+
+procedure unhook(hook_method : uint32);
+var
+    i : uint32;
+begin
+    for i:=0 to MAX_HOOKS-1 do begin
+        If uint32(Hooks[i]) = hook_method then Hooks[i]:= nil;
+        exit;
+    end;
 end;
 
 end.
