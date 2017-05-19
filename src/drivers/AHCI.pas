8022877ea0daf469afe16c62ab6911b1da2140db
aaron <aaron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-19 22:07:49 +0000

    git-svn-id: https://spexeah.com:8443/svn/Asuro@121 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

---
+++ src/drivers/AHCI.pas
@@ -0,0 +1,62 @@
+{ ************************************************
+  * Asuro
+  * Unit: Drivers/AHCI
+  * Description: AHCI SATA Driver
+  ************************************************
+  * Author: Aaron Hance
+  * Contributors: 
+  ************************************************ }
+
+  unit AHCI
+
+  interface
+
+  uses 
+    system,
+    util,
+    isr46;
+
+    type
+
+        TFIS_Type = (
+            REG_H2D, REG_D2H,
+            DMA_ACT, DMA_SETUP,
+            DATA, BIST,
+            PIO_SETUP, DEV_BITS
+        );
+
+        TFIS_REG_H2D bitpacked record
+            fis_type     : uint8;
+            port_mult    : UBit4;
+            rsv0         : UBit3;
+            coc          : boolean;
+            command      : uint8;
+            feature_low  : uint8;
+ 
+            // 
+            lba0         : uint8;
+            lba1         : uint8;
+            lba2         : uint8;
+            device       : uint8;
+ 
+            lba3         : uint8;
+            lba4         : uint8;
+            lba5         : uint8;
+            feature_high : uint8;
+ 
+            lba1         : uint8;
+            lba1         : uint8;
+            lba1         : uint8;
+            lba1         : uint8;
+ 
+            lba1         : uint8;
+
+        end; 
+
+        end;
+
+    var
+
+    implementation 
+
+    end.
