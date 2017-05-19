0bfa418d566c7d04ee3ad4080a454c2114c5c365
aaron <aaron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-05-19 22:13:09 +0000

    git-svn-id: https://spexeah.com:8443/svn/Asuro@123 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/drivers/AHCI.pas
+++ src/drivers/AHCI.pas
@@ -32,28 +32,20 @@
             coc          : boolean;
             command      : uint8;
             feature_low  : uint8;
- 
-            // 
             lba0         : uint8;
             lba1         : uint8;
             lba2         : uint8;
             device       : uint8;
- 
             lba3         : uint8;
             lba4         : uint8;
             lba5         : uint8;
             feature_high : uint8;
- 
             count_low    : uint8;
             count_high   : uint8;
             icc          : uint8;
             control      : uint8;
- 
             rsvl         : uint32;
-
         end; 
-
-        end;
 
     var
 
