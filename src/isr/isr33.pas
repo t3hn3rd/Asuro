6a000366a5ec2a6d1d90f1b00b462c3ef70b6255
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-10-23 17:37:51 +0000

    Keyboard bug traced back to isr33.pas
    git-svn-id: https://spexeah.com:8443/svn/Asuro@231 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/isr/isr33.pas
+++ src/isr/isr33.pas
@@ -32,6 +32,8 @@ var
     b : dword;
 
 begin
+    //writechar('!'); // Bug traces all the way back to here - when the keyboard randomly doesn't work, this inturrupt isn't even called...
+                      // This needs further investigation... Is there something that can go wrong when setting up the PIC?
     b:= inb($60); 
     //console.writehexln(b);
     for i:=0 to MAX_HOOKS-1 do begin
--- src/kernel.pas
+++ src/kernel.pas
@@ -110,6 +110,7 @@ begin
      console.writestringln('DRIVERS: INIT BEGIN.');
      pci.init();
      keyboard.init(keyboard_layout);
+     keyboard.hook(@temphook);
      mouse.init();
      console.writestringln('DRIVERS: INIT END.');
 
@@ -130,7 +131,6 @@ begin
      console.setdefaultattribute(console.combinecolors(White, Black));
      console.writestringln('');
      console.writestringln('Press any key to boot in to Asuro Terminal...');
-     keyboard.hook(@temphook);
      util.halt_and_dont_catch_fire;
 end;
  
