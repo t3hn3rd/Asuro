43e1abf3cb43492d8feaaa92ccf3e9718067db5a
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2017-10-26 11:50:22 +0000

    More cleanup.
    git-svn-id: https://spexeah.com:8443/svn/Asuro@263 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/driver/PCI.pas
+++ src/driver/PCI.pas
@@ -402,18 +402,18 @@ begin
         
 
         if tmp.class_code = 1 then begin
-            console.writestringln('-Device is MASS_STORAGE_CONTROLLER ');
+            console.writestringln('     Device is MASS_STORAGE_CONTROLLER ');
             if tmp.subclass_class = 1 then ATA.init(tmp);
         end; 
         if tmp.class_code = 2 then begin
-            console.writestringln('-Device is NETWORK_CONTROLLER ');
+            console.writestringln('     Device is NETWORK_CONTROLLER ');
         end; 
         if tmp.class_code = 3 then begin
-            console.writestringln('-Device is DISPLAY_CONTROLLER ');
+            console.writestringln('     Device is DISPLAY_CONTROLLER ');
         end; 
         if tmp.class_code = $0C then begin
             if tmp.subclass_class = $03 then begin
-                console.writestringln('-Device is USB Controller');
+                console.writestringln('     Device is USB Controller');
             end;
         end;
     end;
