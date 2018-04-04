d3b5e21acfd061074a2bc07431fabd616b96a01b
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2018-04-04 21:12:39 +0000

    fixed error.
    git-svn-id: https://spexeah.com:8443/svn/Asuro@294 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/driver/PCI.pas
+++ src/driver/PCI.pas
@@ -313,7 +313,7 @@ begin
     console.writehexln(device.prog_if);
 
     drivermanagement.register_device(DevID, @device);
-    kfree(DevID);
+    kfree(void(DevID));
 
     devices[device_count] := device;
     device_count := device_count + 1;
