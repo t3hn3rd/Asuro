a795aa03e20649936e182b3d6e3cefc0a3b4fde3
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2018-04-07 17:28:20 +0000

    git-svn-id: https://spexeah.com:8443/svn/Asuro@379 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/kernel.pas
+++ src/kernel.pas
@@ -152,7 +152,7 @@ begin
      console.setdefaultattribute(console.combinecolors(White, Black));
      console.writestringln('');
      console.writestringln('Press any key to boot in to Asuro Terminal...');
-     BSOD('TOPKEK', 'Because why not?');
+     //BSOD('TOPKEK', 'Because why not?');
      keyboard.hook(@temphook);
 
      util.halt_and_dont_catch_fire;
