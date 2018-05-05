7a7a9667d2d42b67957810e796be06009852219b
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2018-05-05 02:44:45 +0000

    git-svn-id: https://spexeah.com:8443/svn/Asuro@661 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/include/asuro.pas
+++ src/include/asuro.pas
@@ -3,13 +3,13 @@ unit asuro;
 interface
  
 const
-     VERSION       = '1.0.0-657a';
+     VERSION       = '1.0.0-660a';
      VERSION_MAJOR = '1';
      VERSION_MINOR = '0';
      VERSION_SUB   = '0';
-     REVISION      = '657';
+     REVISION      = '660';
      RELEASE       = 'a';
-     LINE_COUNT    = 26781;
+     LINE_COUNT    = 26872;
  
 implementation
  
