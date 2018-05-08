3bdba1d786aa7d34996158fa21dbfbd980da008b
aaron <aaron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2018-05-08 16:29:08 +0000

    git-svn-id: https://spexeah.com:8443/svn/Asuro@676 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/include/asuro.pas
+++ src/include/asuro.pas
@@ -3,20 +3,20 @@ unit asuro;
 interface
  
 const
-     VERSION       = '1.0.1-673ia';
+     VERSION       = '1.0.1-672ia';
      VERSION_MAJOR = '1';
      VERSION_MINOR = '0';
      VERSION_SUB   = '1';
-     REVISION      = '673';
+     REVISION      = '672';
      RELEASE       = 'ia';
-     LINE_COUNT    = 27530;
-     FILE_COUNT    = 88;
-     DRIVER_COUNT  = 32;
+     LINE_COUNT    = 28180;
+     FILE_COUNT    = 90;
+     DRIVER_COUNT  = 34;
      FPC_VERSION   = '2.6.4';
      NASM_VERSION  = '2.10.09';
      MAKE_VERSION  = '3.81';
      COMPILE_DATE  = '08/05/18';
-     COMPILE_TIME  = '01:09:46';
+     COMPILE_TIME  = '10:05:48';
  
 implementation
  
