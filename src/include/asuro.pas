6a4b950da07eab704e0f5ae39143ea1e7476c058
kieron <kieron@6dbc8c32-bb84-406f-8558-d1cf31a0ab0c>
2018-05-10 10:44:41 +0000

    git-svn-id: https://spexeah.com:8443/svn/Asuro@685 6dbc8c32-bb84-406f-8558-d1cf31a0ab0c

--- src/include/asuro.pas
+++ src/include/asuro.pas
@@ -16,7 +16,7 @@ const
      NASM_VERSION  = '2.10.09';
      MAKE_VERSION  = '3.81';
      COMPILE_DATE  = '10/05/18';
-     COMPILE_TIME  = '11:42:59';
+     COMPILE_TIME  = '11:44:37';
  
 implementation
  
