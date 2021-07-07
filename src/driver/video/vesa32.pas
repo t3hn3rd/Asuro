69d1d22a187fb119e2b58c4fb5b2aee46bf4bef4
Kieron Morris <kjm@kieronmorris.me>
2021-07-07 17:32:07 +0100

    Fixed files being weird with case/unit searches

---
+++ src/driver/video/vesa32.pas
@@ -0,0 +1,52 @@
+//  Copyright 2021 Kieron Morris
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+{ 
+	Driver->Video->VESA32 - Implementation of VESA 32bpp draw routines for the VESA Driver.
+	
+	@author(Kieron Morris <kjm@kieronmorris.me>)
+}
+unit vesa32;
+
+interface
+
+uses
+    videotypes, vesa, tracer, color;
+
+//Init the draw routines by providing what we support through the DrawRoutines struct.
+procedure init(DrawRoutines : PDrawRoutines);
+
+implementation
+
+procedure DrawPixel(Buffer : PVideoBuffer; X : uint32; Y : uint32; Pixel : TRGB32);
+var
+    Location : PuInt32;
+    LocationIndex : Uint32;
+
+begin
+    tracer.push_trace('vesa32.DrawPixel.enter');
+    Location:= Puint32(Buffer^.Location);
+    LocationIndex:= (Y * Buffer^.Width) + X;
+    Location[LocationIndex]:= uint32(Pixel);
+    tracer.push_trace('vesa32.DrawPixel.exit');
+end;
+
+procedure init(DrawRoutines : PDrawRoutines);
+begin
+    tracer.push_trace('vesa32.init.enter');
+    DrawRoutines^.DrawPixel:= @DrawPixel;
+    tracer.push_trace('vesa32.init.exit');
+end;
+
+end.
